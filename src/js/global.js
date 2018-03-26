2f0bf02b09dfdf594ee8de2e13d466060c1f9f55
PengYuan <954928769@qq.com>
2018-03-26 18:56:24 +0800

    bug fix

--- src/js/global.js
+++ src/js/global.js
@@ -24,15 +24,15 @@ var global={
      enemySpeed: 2, // 默认敌人移动距离
      enemySize: 50, // 默认敌人的尺寸
      enemyGap: 10,  // 默认敌人之间的间距
-     enemyIcon: '../src/img/enemy.png', // 怪兽的图像
-     enemyBoomIcon: '../src/img/boom.png', // 怪兽死亡的图像
+     enemyIcon: './img/enemy.png', // 怪兽的图像
+     enemyBoomIcon: './img/boom.png', // 怪兽死亡的图像
      enemyDirection: 'right', // 默认敌人一开始往右移动
      planeSpeed: 7, // 默认飞机每一步移动的距离
      planeSize: {
        width: 60,
        height: 100
      }, // 默认飞机的尺寸,
-     planeIcon: '../src/img/plane.png',
+     planeIcon: './img/plane.png',
      score:0,    //游戏得分
      
      plane:{},
@@ -41,7 +41,6 @@ var global={
      nextLevelBtn:nextLevelBtn,
      replayBtn:replayBtn,
      scoreHTML:scoreHTML
-
 }
 
 export{global}
