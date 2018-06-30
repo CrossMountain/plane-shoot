dbfa1edbdff5c91a4fb30bf94c70ab5b51318922
crossmountain <954928769@qq.com>
2018-06-30 09:24:10 +0800

    canvas所要绘制的图形等比例缩放

--- src/js/global.js
+++ src/js/global.js
@@ -7,6 +7,10 @@ var style=window.getComputedStyle(canvas,"")
 canvas.width=parseInt(style.width) 
 canvas.height=parseInt(style.height) 
 
+//比例
+var scale=canvas.width/700
+
+
 var  playBtn=document.querySelector('.js-play')
 var  nextLevelBtn=document.querySelector('.js-next')
 var  replayBtn=document.querySelectorAll('.js-replay')
@@ -24,12 +28,12 @@ var global={
      level: 1, // 游戏默认等级
      totalLevel: 4, // 总共4关
      numPerLine: 6, // 游戏默认每行多少个怪兽
-     canvasPadding: 30, // 默认画布的间隔
-     bulletSize: 10, // 默认子弹长度
-     bulletSpeed: 10, // 默认子弹的移动速度
-     enemySpeed: 2, // 默认敌人移动距离
-     enemySize: 50, // 默认敌人的尺寸
-     enemyGap: 10,  // 默认敌人之间的间距
+     canvasPadding: 30*scale, // 默认画布的间隔
+     bulletSize: 10*scale, // 默认子弹长度
+     bulletSpeed: 10*scale, // 默认子弹的移动速度
+     enemySpeed: 2*scale, // 默认敌人移动距离
+     enemySize: 50*scale, // 默认敌人的尺寸
+     enemyGap: 10*scale,  // 默认敌人之间的间距
      enemyIcon: './img/enemy.png', // 怪兽的图像
      enemyIconImage:'',
 
@@ -37,10 +41,10 @@ var global={
      enemyBoomIconImage:'',
 
      enemyDirection: 'right', // 默认敌人一开始往右移动
-     planeSpeed: 7, // 默认飞机每一步移动的距离
+     planeSpeed: 7*scale, // 默认飞机每一步移动的距离
      planeSize: {
-       width: 60,
-       height: 100
+       width: 60*scale,
+       height: 100*scale
      }, // 默认飞机的尺寸,
      planeIcon: './img/plane.png',
      planeIconImage:'',
